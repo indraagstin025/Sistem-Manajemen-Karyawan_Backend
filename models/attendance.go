0ecd11a8ba36de8c9f4d38e2fe03ac350a7ba5a7
indraagstin025 <indraagustin012@gmail.com>
2025-07-13 21:35:22 +0700

    penyesuaian bagian model kehadiran

--- models/attendance.go
+++ models/attendance.go
@@ -12,7 +12,7 @@ type Attendance struct {
 	Date      string             `json:"date" bson:"date,omitempty"`
 	CheckIn   string             `json:"check_in" bson:"check_in,omitempty"`
 	CheckOut  string             `json:"check_out" bson:"check_out,omitempty"`
-	Status    string             `json:"status" bson:"status,omitempty"`
+	Status    string             `json:"status" bson:"status,omitempty" validate:"omitempty,oneof=Hadir Telat Izin Sakit Cuti Alpha Belum Absen"` 
 	Note      string             `json:"note" bson:"note,omitempty"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at,omitempty"`
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at,omitempty"`
@@ -23,14 +23,14 @@ type AttendanceCreatePayload struct {
 	Date     string `json:"date" validate:"required,datetime=2006-01-02"`
 	CheckIn  string `json:"check_in" validate:"required,datetime=15:04"`
 	CheckOut string `json:"check_out" validate:"omitempty,datetime=15:04"`
-	Status   string `json:"status" validate:"required,oneof=Hadir Telat Izin Sakit Cuti Alpha"`
+	Status   string `json:"status" validate:"required,oneof=Hadir Telat Izin Sakit Cuti Alpha Belum Absen"`
 	Note     string `json:"note"`
 }
 
 type AttendanceUpdatePayload struct {
 	CheckIn  string `json:"check_in,omitempty" validate:"omitempty,datetime=15:04"`
 	CheckOut string `json:"check_out,omitempty" validate:"omitempty,datetime=15:04"`
-	Status   string `json:"status,omitempty" validate:"omitempty,oneof=Hadir Telat Izin Sakit Cuti Alpha"`
+	Status   string `json:"status,omitempty" validate:"omitempty,oneof=Hadir Telat Izin Sakit Cuti Alpha Belum Absen"`
 	Note     string `json:"note,omitempty"`
 }
 
